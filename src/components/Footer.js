b0d29eddef46aedff0d9a5b07e7064424d17cbcc
ivnm <ivnmp1020619738@gmail.com>
2022-03-23 18:28:23 +0800

    make it responsible

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,18 @@
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 const Footer = () => {
   return (
     <footer className='bg-light py-4 mt-auto'>
       <Container>
-        <div className='d-flex justify-content-between'>
-          <span className=''>libdgc.club</span>
-          <span className=''>Made with Next.js, Bootstrap and ✋</span>
-        </div>
+        <Row>
+          <Col md='6' className="mb-2 mb-md-0">
+            <span>libdgc.club</span>
+          </Col>
+          <Col md='6' className="text-md-end" >
+            <span>Made with Next.js, Bootstrap and ✋</span>
+          </Col>
+        </Row>
       </Container>
     </footer>
   );
